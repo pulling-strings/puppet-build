c4d29b2a94363fd414a9ba4b47da1aceffbdc538
narkisr <narkisr@gmail.com>
2012-06-09 23:29:26 +0300

    clearing out plugin and openjdk module, updating gradle to rc3

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,52 +1,38 @@
 # Class: gradle
 #
-# Requires: JDK 1.5
-#
 #
 class gradle {
-  include 'openjdk'
 
-  $version = 'gradle-1.0-rc-1'
-  
+  $version = 'gradle-1.0-rc-3'
+
+  package{"unzip": ensure  => present }
+
   Exec {
-	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
   }
 
   archive { $version:
     ensure	=> present,
-	url 	=> "http://downloads.gradle.org/distributions/$version-all.zip",
-	checksum	=> false,
-	src_target	=> '/tmp',
-	target	=> '/usr/share',
-	extension	=> 'zip',
+    url 	=> "http://downloads.gradle.org/distributions/${version}-all.zip",
+    checksum	=> false,
+    src_target	=> '/tmp',
+    target	=> '/usr/share',
+    extension	=> 'zip',
+    require  => Package['unzip']
   }
-  
+
   file { '/usr/share/gradle':
     ensure	=> link,
-	target	=> "/usr/share/$version",
-	owner	=> root, group	=> root,
-	require	=> Archive["$version"],
+    target	=> "/usr/share/$version",
+    owner	=> root, group	=> root,
+    require	=> Archive["$version"],
   }
 
   file { '/etc/profile.d/gradle.sh':
-	ensure	=> file,
-	mode	=> 644,
-	source	=> 'puppet:///modules/gradle/gradle.sh',
-	owner	=> root, group	=> root,
+    ensure	=> file,
+    mode	=> 644,
+    source	=> 'puppet:///modules/gradle/gradle.sh',
+    owner	=> root, group	=> root,
   }
-  
-  # Prototypes Plugin
-  archive::download { 'prototypes.jar':
-	url => 'https://github.com/downloads/myrontuttle/gradle-prototypes/prototypes.jar',
-	ensure => present,
-	src_target => '/usr/share/gradle/lib/plugins',
-	require => File['/usr/share/gradle']
-  }
-  
-  archive::download { 'prototypes.gradle':
-	url => 'https://github.com/downloads/myrontuttle/gradle-prototypes/prototypes.gradle',
-	ensure => present,
-	src_target => '/usr/share/gradle/init.d',
-	require => Archive::Download['prototypes.jar']
-  }
+
 }
