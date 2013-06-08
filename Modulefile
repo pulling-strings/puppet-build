c6209a895426d119f48a404b29e05eb71bddad7c
Ronen Narkis <narkisr@gmail.com>
2013-06-09 01:12:30 +0300

    adding barbecue and module file

---
+++ Modulefile
@@ -0,0 +1,12 @@
+name    'narkisr-build'
+version '0.1.0'
+source 'git://github.com/narkisr/puppet-build.git'
+author 'Ronen Narkis narkisr@gmail.com'
+license 'Apache License, Version 2.0'
+summary 'Build utilities'
+description 'Sets up a list of build utilities (mainly for JVM based langs).'
+project_page 'https://github.com/narkisr/puppet-build'
+
+## Add dependencies, if any:
+dependency 'narkisr/barbecue', '>= 0.1.0'
+# dependency 'camptocamp / puppet-archive', '>= 0.1.0'
--- manifests/lein.pp
+++ manifests/lein.pp
@@ -6,22 +6,13 @@
 # [Remember: No empty lines between comments and class definition]
 class build::lein {
 
-  $lein_url = 'https://raw.github.com/technomancy/leiningen/preview/bin/lein'
-
   package{'openjdk-6-jdk':
     ensure  => present
   }
 
-  apt::source { 'narkisr-debs':
-    location    => 'http://dl.bintray.com/content/narkisr/narkisr-debs',
-    repos       => '/',
-    release     => '',
-    include_src => false,
-  } ->
-
   package{ 'leiningen':
     ensure   => installed,
-    require  => Class['jdk']
+    require  => [Class['jdk'], Class['barbecue']]
   }
 
 }
