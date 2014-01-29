81e55872c581cbac14b125efc7208ec823de0175
narkisr <narkisr@gmail.com>
2014-01-30 00:11:34 +0200

    to moving to gradle 1.10

--- manifests/gradle.pp
+++ manifests/gradle.pp
@@ -3,7 +3,7 @@
 #
 class build::gradle {
 
-  $version = 'gradle-1.7'
+  $version = 'gradle-1.10'
 
   package{'unzip': ensure  => present }
 
@@ -11,12 +11,12 @@ class build::gradle {
     path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   }
 
-  $url = "http://downloads.gradle.org/distributions/${version}-all.zip"
+  $url = "http://downloads.gradle.org/distributions/${version}-bin.zip"
 
   archive { $version:
     ensure        => present,
     url           => $url,
-    digest_string => '4317dbbd5d860e799571342deab9c9cc',
+    digest_string => '5594e5bc7f088683c1e90f99aa49f5fc',
     src_target    => '/opt',
     target        => '/usr/share',
     extension     => 'zip',
