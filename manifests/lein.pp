f77e53a20a318216f3b0dd0c1cac15a0e4a92630
narkisr <narkisr@gmail.com>
2015-12-08 15:47:43 +0200

    fixing permissions

--- manifests/lein.pp
+++ manifests/lein.pp
@@ -7,8 +7,12 @@
 class build::lein {
 
   package{ 'leiningen':
-    ensure   => installed,
-    require  => [Class['jdk'], Class['barbecue']]
+    ensure  => installed,
+    require => [Class['jdk'], Class['barbecue'], Class['apt::update']]
+  } ->
+
+  file{'/usr/bin/lein':
+    mode  => 'a+rwx'
   }
 
 }
